056f4e10f05b4efecda6254d94c22533ae52d621
Furkan Ayakdaş <72611180+furkan0920@users.noreply.github.com>
2025-05-29 01:30:57 +0300

    footer translation

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { useTranslation } from 'react-i18next';
 import ins1 from '../../assets/Footer/buggy.jpg'
 import ins2 from '../../assets/Footer/fishing.jpg'
 import ins3 from '../../assets/Footer/lavender.jpg'
@@ -7,27 +8,28 @@ import ins4 from '../../assets/Footer/rafting.jpg'
 import ins5 from '../../assets/Footer/zipline.jpg'
 
 function Footer() {
+    const { t } = useTranslation();
     return (
         <>
-            <footer className="bg-light py-5  pb-0">
+            <footer className="bg-light py-5 pb-0">
                 <div className="container">
                     <div className="row g-4">
-                        <h2 className="fw-bold mb-4">Hızlı Bağlantılar:</h2>
+                        <h2 className="fw-bold mb-4">{t('footer.quick_links')}</h2>
                         <div className="row g-3 w-100">
                             <div className="col-md-3">
-                                <p><i className="fas fa-check me-2"></i>Rafting Turları</p>
-                                <p><i className="fas fa-check me-2"></i>ATV (Quad) Safari</p>
-                                <p><i className="fas fa-check me-2"></i>Balık Tutma Turları</p>
+                                <p><i className="fas fa-check me-2"></i>{t('footer.rafting')}</p>
+                                <p><i className="fas fa-check me-2"></i>{t('footer.quad')}</p>
+                                <p><i className="fas fa-check me-2"></i>{t('footer.fishing')}</p>
                             </div>
                             <div className="col-md-3">
-                                <p><i className="fas fa-check me-2"></i>Buggy Safari</p>
-                                <p><i className="fas fa-check me-2"></i>Jeep Safari</p>
-                                <p><i className="fas fa-check me-2"></i>Lavanta Bahçeleri Gezisi</p>
+                                <p><i className="fas fa-check me-2"></i>{t('footer.buggy')}</p>
+                                <p><i className="fas fa-check me-2"></i>{t('footer.jeep')}</p>
+                                <p><i className="fas fa-check me-2"></i>{t('footer.lavender')}</p>
                             </div>
                             <div className="col-md-3">
-                                <p><i className="fas fa-check me-2"></i>Tazı Kanyonu Turu</p>
-                                <p><i className="fas fa-check me-2"></i>Macera Parkı</p>
-                                <p><i className="fas fa-check me-2"></i>Zipline Deneyimi</p>
+                                <p><i className="fas fa-check me-2"></i>{t('footer.tazi')}</p>
+                                <p><i className="fas fa-check me-2"></i>{t('footer.adventure_park')}</p>
+                                <p><i className="fas fa-check me-2"></i>{t('footer.zipline')}</p>
                             </div>
                             <div className="footer-social col-md-3">
                                 <p><i className="fas fa-phone-alt me-2"></i>+90 533 153 02 29</p>
